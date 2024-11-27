b4faa57d22014a01f4e12c2e586ed3b2c1cb21d3
Camelia-hu <3225664565@qq.com>
2024-11-27 20:06:03 +0800

    重新设计order数据表

--- order/handler.go
+++ order/handler.go
@@ -5,7 +5,9 @@ import (
 	"github.com/Camelia-hu/gomall/dao"
 	"github.com/Camelia-hu/gomall/module"
 	order "github.com/Camelia-hu/gomall/order/kitex_gen/order"
+	"gorm.io/gorm"
 	"log"
+	"strconv"
 )
 
 // OrderServiceImpl implements the last service interface defined in the IDL.
@@ -13,23 +15,34 @@ type OrderServiceImpl struct{}
 
 // PlaceOrder implements the OrderServiceImpl interface.
 func (s *OrderServiceImpl) PlaceOrder(ctx context.Context, req *order.PlaceOrderReq) (resp *order.PlaceOrderResp, err error) {
-	var orders []*module.Order
+	var ReqOrder module.Order
 	address := req.Address.Country + req.Address.State + req.Address.City + req.Address.StreetAddress
-	for _, item := range req.OrderItems {
-		Order := &module.Order{
-			Uid:          req.UserId,
-			UserCurrency: req.UserCurrency,
-			Address:      address,
-			Email:        req.Email,
-		}
-		orders = append(orders, Order)
+	ReqOrder = module.Order{
+		Model:        gorm.Model{},
+		Uid:          req.UserId,
+		UserCurrency: req.UserCurrency,
+		Address:      address,
+		Email:        req.Email,
 	}
-	err = dao.DB.Create(orders).Error
+	err = dao.DB.Create(&ReqOrder).Error
 	if err != nil {
-		log.Println("order create err : ", err)
 		return nil, err
 	}
-	resp = &order.PlaceOrderResp{Order: &order.OrderResult{OrderId: Uuid}}
+	for _, item := range req.OrderItems {
+		oneItem := module.OrderItem{
+			Model:     gorm.Model{},
+			OrderID:   ReqOrder.ID,
+			Cost:      item.Cost,
+			ProductId: item.Item.ProductId,
+			Quantity:  item.Item.Quantity,
+		}
+		err = dao.DB.Create(oneItem).Error
+		if err != nil {
+			log.Println("order item create err : ", err)
+			return nil, err
+		}
+	}
+	resp = &order.PlaceOrderResp{Order: &order.OrderResult{OrderId: strconv.Itoa(int(ReqOrder.ID))}}
 	return resp, nil
 }
 
@@ -44,7 +57,7 @@ func (s *OrderServiceImpl) ListOrder(ctx context.Context, req *order.ListOrderRe
 	var Orders []*order.Order
 	for _, o := range orders {
 		oneOrder := &order.Order{
-			OrderId:      o.UUid,
+			OrderId:      strconv.Itoa(int(o.ID)),
 			UserId:       o.Uid,
 			UserCurrency: o.UserCurrency,
 			Email:        o.Email,
